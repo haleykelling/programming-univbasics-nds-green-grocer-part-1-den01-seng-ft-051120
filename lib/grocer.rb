8c30d14f103eae654de329572c8eef86214e759e
Haley Kelling <haley@kellings.com>
2020-05-04 16:26:24 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,7 +2,7 @@ def find_item_by_name_in_collection(name, collection)
   count = 0
   while count < collection.length do
     if collection[count][:item] == name
-      collection[count]
+      return collection[count]
     end
     count += 1
   end
